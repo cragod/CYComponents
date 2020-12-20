a9e93e49fe57919aa60e5bf384534fcfe2279b9c
Gatro CY <cragodn@gmail.com>
2020-12-20 19:48:17 +0800

    contract coin pair 灵活配置

--- cy_components/utils/coin_pair.py
+++ cy_components/utils/coin_pair.py
@@ -42,10 +42,13 @@ class CoinPair:
 
 
 class ContractCoinPair(CoinPair):
+
+    sep = None
+
     """合约币对"""
     @classmethod
     def coin_pair_with(cls, cp_str, sep='/'):
-        comps = cp_str.split(sep, 3)
+        comps = cp_str.split(sep, cp_str.count(sep))
 
         def safe_list_get(l, idx, default=None):
             try:
@@ -55,13 +58,15 @@ class ContractCoinPair(CoinPair):
         if len(comps) < 2:
             return None
 
-        return ContractCoinPair(comps[0], comps[1], safe_list_get(comps, 2))
+        cp = ContractCoinPair(comps[0], comps[1], safe_list_get(comps, 2))
+        cp.sep = sep
+        return cp
 
     def __init__(self, trade_coin='', base_coin='', tail_str='', custom_min_cost=None):
         super().__init__(trade_coin, base_coin, custom_min_cost)
-        self.tail = tail_str
+        self.tail = tail_str.upper() if tail_str is not None else None
 
-    def formatted(self, sep='/'):
+    def formatted(self, sep=None):
         """获取格式化的币对
 
         Parameters
@@ -69,6 +74,7 @@ class ContractCoinPair(CoinPair):
         sep : str, optional
             分割符, by default '/'
         """
+        sep = sep if sep is not None else self.sep if self.sep is not None else '/'
         base_str = "{}{}{}".format(self.trade_coin, sep, self.base_coin)
         if self.tail is not None:
             base_str = "{}{}{}".format(base_str, sep, self.tail)
