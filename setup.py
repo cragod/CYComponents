444cae86fd5589da431ae70888f63bf2076fa10e
Talon.Huang <talon.huang@ringcentral.com>
2021-12-16 20:37:02 +0800

    Bump version: 0.3.16 → 0.3.17

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/cragod/CYComponents',
-    version='0.3.16',
+    version='0.3.17',
     zip_safe=False,
 )
